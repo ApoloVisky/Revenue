0f930493a4006ec9d1162c7b42414d8e0981bc2d
Adeilton da Silva Polovodoff <78828936+ApoloVisky@users.noreply.github.com>
2026-04-13 13:24:03 +0000

    alter

--- Frontend/pages/index.js
+++ Frontend/pages/index.js
@@ -3,7 +3,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const API = process.env.NEXT_PUBLIC_API_URL;
+const API = "revenue-production.up.railway.app";
 
 export default function Home() {
   const [screen, setScreen] = useState("login");
